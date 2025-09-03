f33efce45c3a699832ebf328a8173b0a404a308f
Kabir <shivamvats@Shivams-MacBook-Air.local>
2025-09-04 01:36:21 +0530

    Add skeletion in backend to support private chat functionality

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,55 @@
-import { WebSocketServer } from "ws";
+import WebSocket, { WebSocketServer } from "ws";
+
+interface User {
+  socket: WebSocket,
+  room: string
+}
 
 const websocketServer = new WebSocketServer({ port: 8000 });
 
-websocketServer.on("connection", (socket) => {
-  socket.send("Helooo from server!");
-  console.log("Client connected!");
+let globalArr: User[] = [];
 
-  socket.on("message", (e) => {
-    if (e.toString() === "ping") {
-      socket.send("pong");
-    }
-  });
+websocketServer.on("connection", (socket) => {
+  socket.send("Helooo from WebSocket!");
+    socket.on("message", (msg) => {
+      // @ts-ignore
+      const parsedMsg = JSON.parse(msg);
+      /*
+        Request to Join a Room:
+          {
+             "type": "join",
+             "payload": {
+               "roomId": "123"
+             }
+          }
+      */
+      if (parsedMsg.type == "join") {
+        globalArr.push({
+          socket: socket,
+          room: parsedMsg.payload.roomId
+        })
+      } else {
+        /*
+        Send a message in a Room:
+        {
+          "type": "chat",
+          "payload": {
+            "message: "hi there"
+          }
+        }
+        */
+        const currentUserRoom = globalArr.find((x) => x.socket == socket)?.room;
+
+        if (!currentUserRoom) {
+          socket.send("Not part of room, join one!")
+        } else {
+          for (let index = 0; index < globalArr.length; index++) {
+            if (globalArr[index]?.room == currentUserRoom) {
+              globalArr[index]?.socket.send(parsedMsg.payload.message)
+            }
+          }
+        }
+      }
+    });
 })
 
